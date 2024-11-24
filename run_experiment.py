2dbd07b6029ccdd74080f1df42548b2d10841050
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-11-23 19:05:25 -0500

    ran MS MARCO subsets on alibaba and miniLM - works

--- run_experiment.py
+++ run_experiment.py
@@ -2,6 +2,6 @@ from experiment_manager import ExperimentManager
 
 if __name__ == "__main__":
     
-    exp_dir = "experiments/nov_18_setup/trial2"
+    exp_dir = "experiments/nov_18_setup/trial2_miniLM"
     experiment_manager = ExperimentManager()
     experiment_manager.run_experiment(exp_dir)
