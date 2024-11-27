1953aeb38d64e28acf2ad51e5bdd5dff557e93b6
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-11-27 16:28:52 -0500

    general agent and policy classes work

--- search_agent/__init__.py
+++ search_agent/__init__.py
@@ -1,24 +1,2 @@
-from .general_agent import GeneralAgent
-from .dense_retriever import DenseRetriever
-from .policies import PipelinePolicy
-import embedding
-
-
-#Classes that function as agents (have a rank method)
-AGENT_CLASSES = {
-    'DenseRetriever': DenseRetriever,
-    'GeneralAgent': GeneralAgent
-    }
-
-#Classes that might be used as components in a general agent (QPP, Embedders, LLMs, etc)
-COMPONENT_CLASSES = {}
-COMPONENT_CLASSES.update(AGENT_CLASSES)
-
-#"Main" methods for a class. E.g. rank() for a BaseAgent, query_performance_prediction() for QPP, etc  
-#MAIN_ACTIONS = {
-#    DenseRetriever: DenseRetriever.rank 
-#    }
-
-POLICY_CLASSES = {
-    'PipelinePolicy' : PipelinePolicy
-    }
+from .registry import AGENT_CLASSES, COMPONENT_CLASSES, POLICY_CLASSES
+#import embedding
