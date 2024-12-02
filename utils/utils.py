c5f80ee5e09fd0b1bc7e7d8081571fd157a3184c
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-12-02 10:48:40 -0500

    Fix bug in get_doc_test_list -- wasn't preserving list order

--- utils/utils.py
+++ utils/utils.py
@@ -5,11 +5,18 @@ def get_doc_text_list(ids, corpus_path):
     #ids: list of doc_ids
     #return [{id: d1, text: <text_d1>},...]
 
-    id_and_text_list = []
+    id_set = set(ids)
+    id_and_text_dict = {}
 
+    # Iterate through the corpus and collect only the required documents
     for doc in jsonl_line_generator(corpus_path):
-        if doc.get('docID') in ids:
-            id_and_text_list.append({'docID': doc.get('docID'), 'text': doc.get('text')})
+        doc_id = doc.get('docID')
+        if doc_id in id_set:
+            id_and_text_dict[doc_id] = doc.get('text')
+            if len(id_and_text_dict) == len(ids):
+                break
+
+    id_and_text_list = [{'docID': doc_id, 'text': id_and_text_dict[doc_id]} for doc_id in ids if doc_id in id_and_text_dict]
 
     return id_and_text_list
 
