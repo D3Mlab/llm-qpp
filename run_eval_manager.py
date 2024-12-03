1c13c76b238b28451861bf4bf1b3ade7211abf52
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-12-02 22:53:34 -0500

    wayfair first 5

--- run_eval_manager.py
+++ run_eval_manager.py
@@ -3,7 +3,7 @@ from eval_manager import EvalManager
 if __name__ == "__main__":
     
     eval_manager = EvalManager()
-    exp_dir = "experiments/nov_18_setup/trial2_MS_Marco_10K_miniLM_listwise_rr"
+    exp_dir = "experiments/dec_2/wayfair_20_ali"
     eval_manager.eval_experiment(exp_dir)
 
     #exp_dir = "experiments/nov_18_setup"
