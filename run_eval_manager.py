e6ea4903ed34f7358bf7329d3d89b673b893fb20
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-12-03 13:34:23 -0500

    -

--- run_eval_manager.py
+++ run_eval_manager.py
@@ -3,7 +3,7 @@ from eval_manager import EvalManager
 if __name__ == "__main__":
     
     eval_manager = EvalManager()
-    exp_dir = "experiments/dec_2/wayfair_20_ali_k10"
+    exp_dir = "experiments/dec_2/wayfair_20_ali_flash_rerank10_ret30_T3"
     eval_manager.eval_experiment(exp_dir)
 
     #exp_dir = "experiments/nov_18_setup"
