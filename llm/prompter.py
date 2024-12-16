cb9b8a17eb1500cec1b604386322dbb4dba22d7a
Anton Korikov <anton.korikov@mail.utoronto.ca>
2024-12-16 10:34:37 -0500

    comments

--- llm/prompter.py
+++ llm/prompter.py
@@ -45,11 +45,12 @@ class Prompter():
         #given a) the previously best list of length K and b) K additional retrieved docs
         #rerank these 2xK docs to a list of length K
 
+
+        #could rewrite based on a slice to take of the retrieved docs (could set very high like 200)
         curr_top_k_docIDs = state.get("curr_top_k_docIDs", [])
         last_k_retrieved_docIDs = state["retrieved_lists"][-1]
         doc_ids = curr_top_k_docIDs + last_k_retrieved_docIDs
         k = self.config['rerank'].get('k')
-
 
         corpus_path = self.config['data_paths']['corpus_text_path']
         doc_ids_and_texts = get_doc_text_list(doc_ids,corpus_path)
